e64165d7925f2abcf11f12947b9871deb0eaedf2
bebersdocks <bebersdocks@protonmail.com>
2021-04-30 10:12:59 +0300

    common.ml zero level pattern matching vbars added

--- utility/common.ml
+++ utility/common.ml
@@ -3,13 +3,13 @@ open Str
 let char_list_of_string s = List.init (String.length s ) (String.get s)
 
 let rec string_of_char_list = function 
-    [] -> ""
+  | [] -> ""
   | hd :: tl -> (String.make 1 hd) ^ string_of_char_list tl
 
 let trim_char str = 
   let char_list = char_list_of_string str in 
   let rec trim_char_from_list = function
-      [] -> []
+    |  [] -> []
     | hd :: tl -> if hd = '\n' then trim_char_from_list tl else hd :: trim_char_from_list tl in
   let trimmed = trim_char_from_list char_list in 
   string_of_char_list trimmed
