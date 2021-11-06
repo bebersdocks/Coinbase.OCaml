12ba322f67f6e7be5aa15b7b99d5076abbf69b86
bebersdocks <bebersdocks@protonmail.com>
2021-11-06 16:49:19 +0200

    Use json for settings

--- coinbase/authentication.ml
+++ coinbase/authentication.ml
@@ -5,7 +5,7 @@ open Unix
     
 let generate_signature timestamp http_method path body = 
   let message = timestamp ^ http_method ^ path ^ body in
-  let hmac_key = transform_string (Base64.decode()) Settings.secret in 
+  let hmac_key = transform_string (Base64.decode()) settings.secret in 
   let signature = hash_string (MAC.hmac_sha256 hmac_key) message in 
   let signature_base64 = 
     transform_string (Base64.encode_multiline()) signature in
@@ -17,8 +17,8 @@ let headers request http_method body =
   [
     "CB-ACCESS-SIGN", signature;
     "CB-ACCESS-TIMESTAMP", timestamp;
-    "CB-ACCESS-KEY", Settings.api_key;
-    "CB-ACCESS-PASSPHRASE", Settings.passphrase;
+    "CB-ACCESS-KEY", settings.api_key;
+    "CB-ACCESS-PASSPHRASE", settings.passphrase;
     "Content-Type", "application/json";
   ]
 
--- utility/settings.ml
+++ utility/settings.ml
@@ -1,6 +1,13 @@
-open Xml
+open Yojson.Basic
+open Yojson.Basic.Util
 
-(* TODO: read from config *)
-let passphrase = ""
-let secret = ""
-let api_key = ""
+type settings = 
+  { api_key: string; 
+    secret: string; 
+    passphrase: string; }
+
+let settings =
+  let settings_json = from_file "settings.json" in
+  { api_key = settings_json |> member "apiKey" |> to_string;
+    secret = settings_json |> member "secret" |> to_string;
+    passphrase = settings_json |> member "passphrase" |> to_string; }
